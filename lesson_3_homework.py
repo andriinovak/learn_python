36957eca18e5578b36600ecce7331b7d36449320
andriinovak <andrii.novak91@gmail.com>
2019-10-07 17:13:04 +0300

    Create lesson_3_homework.py

---
+++ lesson_3_homework.py
@@ -0,0 +1,20 @@
+number = input('Phone number: ')
+if len(number) == 10 and number.isdigit() == True:
+	print('This is number')
+else:
+	print('This is not number')
+print()
+ans = input('5 + 5 = ')
+ans = int(ans)
+if ans == 10:
+	print('You are right')
+else:
+	print('You are wrong')
+print()
+a = ('andrii')
+b = input('enter your name: ')
+b = b.lower()
+if a == b:
+	print('true')
+else:
+	print('false')
